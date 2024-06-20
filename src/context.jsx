78ad092a9cae76a8dbffdc044ea0a32e5580620b
bharathi natarajan <bharathi_n@msn.com>
2024-06-20 22:05:14 +0530

    initial setup

---
+++ src/context.jsx
@@ -0,0 +1,9 @@
+import { useContext, createContext, useState } from 'react'
+
+const AppContext = createContext()
+
+export const AppProvider = ({ children }) => {
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>
+}
+
+export const useGlobalContext = () => useContext(AppContext)
